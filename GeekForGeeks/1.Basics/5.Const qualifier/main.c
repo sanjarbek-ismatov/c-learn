1a8c74456a0fa732f5bab9dba0f255fa94fa3599
Sanjarbek <ismatovsanjarbek@yandex.ru>
2024-03-21 10:01:20 +0500

    5.Constant qualifier

---
+++ GeekForGeeks/1.Basics/5.Const qualifier/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void){
+    int const num1 = 18;
+    const int num2 = 19;
+    // int const* const ptr = &num2;
+    const int* const ptr = &num2;
+    // *ptr = 10;
+    // ptr = & num2;
+    printf("%d\n", *ptr);
+    return 0;
+}
