db201f830cbd93e3301ec8a385b5a8a8814a54c0
sanjarbek-ismatov <ismatovsanjarbek@yandex.ru>
2024-04-15 18:50:43 +0500

    1108: fault

--- Problems/Leetcode/1108. Defanging an IP Address/main.c
+++ Problems/Leetcode/1108. Defanging an IP Address/main.c
@@ -1,13 +1,35 @@
 #include<stdio.h>
-void insert(char string[], int position, char c){
-    printf('%s\n', string);
-    for(int i = 0; i <= position; i++){
-        string[i] = c;
+#include<string.h>
+#include<stdlib.h>
+void shift(char str[], int position, char shifting_c){
+    char current = shifting_c;
+    char next;
+    for(int i = position; i < strlen(str); i++){
+        next = str[i + 1];
+        str[i + 1] = current;
+        current = next;
     }
 }
+
+void insert(char str[], int position, char c){
+    shift(str, position, str[position]);
+    str[position] = c;
+}
+char * defangIPaddr(char * address){
+    char addr[strlen(address) + 1];
+    strcpy(addr, address);
+    for(int i = 0; i < strlen(address); i++){
+        if(addr[i] == '.'){
+            insert(addr, i, '[');
+            insert(addr, i + 2, ']');
+            i += 2;
+        }
+    }
+    char* res = malloc(strlen(addr) + 1);
+    strcpy(res, addr);
+    return res;
+}
 int main(){
-    char name[] = "Sanjarbek";
-    insert(name, 0, 'C');
-    printf("%s\n", name);
+    printf("%s\n", defangIPaddr("255.100.50.0")); // it should print: 255[.]100[.]50[.]0
     return 0;
 }
