3e88f110f72dbda544bdfcc1e95d0d8d45a7480d
sanjarbek-ismatov <ismatovsanjarbek@yandex.ru>
2024-05-29 09:19:52 +0500

    I/O is finished

--- GeekForGeeks/3.Input and Output/5.Formatted and Unformatted IO/unformatted.c
+++ GeekForGeeks/3.Input and Output/5.Formatted and Unformatted IO/unformatted.c
@@ -4,17 +4,19 @@ int main(){
     printf("Hey, I just want to get you some details!\nTell about yourself\n");
     printf("Do you agree? Y/N: ");
     // getch only get input 
-    // getche displays
+    // getche displays as well
      char info[128];
     char res = getche();
 
     if(res == 'Y'){
         printf("\nWhat your name then: ");
-        scanf("%s", info);
-        printf("%s\n", info);
+        gets(info);
+        puts(info);
+        // printf("%s\n", info);
     } else{
         printf("Cancelled by the user!\n");
     }
+    putch(res);
     
     return 0;
 }
