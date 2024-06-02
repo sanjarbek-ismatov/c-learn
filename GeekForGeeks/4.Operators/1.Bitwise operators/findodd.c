8e916a9e5760cbe28537a87b62e7d1122e8e827f
sanjarbek-ismatov <ismatovsanjarbek@yandex.ru>
2024-06-02 06:52:47 +0500

    bitwise operators

--- GeekForGeeks/4.Operators/1.Bitwise operators/findodd.c
+++ GeekForGeeks/4.Operators/1.Bitwise operators/findodd.c
@@ -1,17 +1,22 @@
 #include<stdio.h>
-
+#include<stdbool.h>
 int find_odd(const int arr[], int size){
     int res = 0;
     for(int i = 0; i < size; i++){
         res ^= arr[i];
-        printf("%d %d\n", res, arr[i]);
     }
     return res;
 }
 
+bool is_even(int number){
+    return (number & 1) ? false : true; 
+}
+
+
 int main(){
-    const int arr[] = {34, 88, 24, 40, 23, 54, 78, 50, 38, 20, 94};
-    const int res = find_odd(arr, sizeof(arr) / sizeof(arr[0]));
-    printf("%d\n", res);
+    // const int arr[] = {40, 88, 88, 24, 40, 38, 23, 54, 24, 40, 38, 20, 20, 54, 54};
+    // const int res = find_odd(arr, sizeof(arr) / sizeof(arr[0]));
+    printf("%d\n", is_even(26));
+    printf("%d", ~-25);
     return 0;
 }
