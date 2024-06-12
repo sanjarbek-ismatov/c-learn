ff7e444631f9323238839a192085f132eecd22a3
sanjarbek-ismatov <ismatovsanjarbek@yandex.ru>
2024-06-12 11:09:07 +0500

    argument helper

--- GeekForGeeks/6.Functions/3.Variadic functions/main.c
+++ GeekForGeeks/6.Functions/3.Variadic functions/main.c
@@ -1,15 +1,32 @@
 #include<stdarg.h>
 #include<stdio.h>
+#include<stdlib.h>
+
+int* arguments(va_list ptr, int n){
+    int* arr = (int*)malloc(n * sizeof(int));
+    if(arr == NULL){
+        fprintf(stderr, "Memory Allocation Failed!\n");
+        return NULL;
+    }
+     for(int i = 0; i < n; i++){
+        arr[i] = va_arg(ptr, int);
+    }
+    return arr;
+}
 int sum(int n, ...){
     va_list ptr;
     va_start(ptr, n);
+    int* args = arguments(ptr, n);
+    va_end(ptr);
     int sum = 0;
     for(int i = 0; i < n; i++){
-        sum += va_arg(ptr, int);
+        sum += args[i];
     }
-    va_end(ptr);
+    free(args);
     return sum;
 }
+
+
 
 int main(){
     const int res = sum(3, 2, 3, 4);
