36a21e44e5ee9b5317a59b55a6470def55aeee06
sanjarbek-ismatov <ismatovsanjarbek@yandex.ru>
2024-06-27 15:03:05 +0500

    Math.h

---
+++ GeekForGeeks/6.Functions/6.Math.h/main.c
@@ -0,0 +1,18 @@
+#include<math.h>
+#include<stdio.h>
+int main(){
+    double PI = 3.14;
+    printf("ceil is: %.1lf, floor is: %.1lf\n", ceil(PI), floor(PI));
+    int a = -10;
+    double b = -3.14;
+    float c = 5.5;
+    // Absolute
+    printf("%.1f %.2f %.1f\n", fabs(a), fabs(b), fabs(c));
+    // Logarithm
+    printf("%lf\n", log2(1024));
+    // Module
+    printf("11 %% 3 = 2\n", fmod(11, 3));
+    // Power
+    printf("%.0lf", pow(2, 3));
+    return 0;
+}
