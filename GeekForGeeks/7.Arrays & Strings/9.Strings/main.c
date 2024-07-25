f36673c0181d63cdfafaa8db65bd85f6a9f1400b
sanjarbek-ismatov <ismatovsanjarbek@yandex.ru>
2024-07-25 14:59:05 +0500

    genius thing

--- GeekForGeeks/7.Arrays & Strings/9.Strings/main.c
+++ GeekForGeeks/7.Arrays & Strings/9.Strings/main.c
@@ -1,8 +1,24 @@
+#include<string.h>
 #include<stdio.h>
 int main(){
-    char name[50];
+    char name[50] = "Sanjarbek";
     // fgets(name, 50, stdin);
-    scanf("%[^\n]s", name);
+    // scanf("%[^\n]s", name);
+    // gets();
+
+    char last[] = "Ismatov";
+    strcat(name, " ");
+    strcat(name, last);
+    // printf("%s", strupr(name));
+
+    char word_to_be_removed[] = "bek";
+    char* ptr = strstr(name, word_to_be_removed);
+    char* ptr2 = ptr + strlen(word_to_be_removed);
+    for(int i = 0; i < strlen(ptr2); i++){
+        ptr = ptr2++;
+        ptr++;
+    }
+
     printf("%s", name);
     return 0;
 }
