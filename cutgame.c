5ca680beb0c40b06f1f4969a018f18ef07c4bb60
Sanjarbek <ismatovsanjarbek@yandex.ru>
2024-12-07 21:48:01 +0500

    started to review

--- cutgame.c
+++ cutgame.c
@@ -1,97 +1,57 @@
-// This library for input and output
 #include <stdio.h>
-
-// This library includes functions such as memory allocating ones
-
 #include <stdlib.h>
-
-// To declare boolean
-
 #include <stdbool.h>
-
-// For string
-
 #include <string.h>
-
-// This's been used for sleep function
-
 #include <time.h>
-
-// To analyze type of values
-
 #include <ctype.h>
 
 // this was my code, wow, But I can't remember it, howwwww???
 
-/**
- * get_random_character function returns either m or b or q
-*/
-
 char get_random_character() {
-    // in every calls, random_index is defined as a number from 0 to 2 
-
-    int random_index = rand() % 3;
-
-    // An array to store f - fist, c - cloud, s - a pair of scissors
-    char sym[3] = {
-            'f',
+    short unsigned int random_index = rand() % 3;
+    char sym[] = {
+            'p',
             'c',
             's'
     };
-    // printf("%d\n", random_index);
- 
-    // Returns a random item of the array
     return sym[random_index];
 }
 
-
-bool checkWhichWon(char * string) {
+bool random_character(char * string) {
     if (isupper(string[0])) string[0] = tolower(string[0]);
     char * toString = malloc(10);
     char randomResult = get_random_character();
     switch (randomResult) {
-        case 'm':
-            strcpy(toString, "musht");
+        case 'p':
+            strcpy(toString, "paper");
             break;
-        case 'b':
-            strcpy(toString, "bulut");
+        case 'c':
+            strcpy(toString, "stone");
             break;
-        case 'q':
-            strcpy(toString, "qaychi");
+        case 's':
+            strcpy(toString, "scissors");
             break;
         default:
             strcpy(toString, "");
     }
-
-    return strcmp(toString, string) == 0;
+    // return strcmp(toString, string) == 0;
 }
-char * converter(int num) {
 
+char* converter(int num) {
     return num == 1 ? "Tabriklayman Yutdingiz!!" : "Afsuski xato";
 }
+
 int main() {
-    int score[2] = {
-            0,
-            0
-    };
+    int score[2] = {0,0};
     char * input = malloc(10);
     char * name = malloc(50);
     char buffer[100];
-
-    FILE* file = fopen("data.txt", "rw");
-    char* local_name = fgets(buffer,30, file);
-    if(!local_name){
-            printf("Salom ismingiz: ");
-            scanf("%s", name);
-        fprintf(file, name);
-        local_name = fgets(buffer, 30, file);
-    }
-    printf("Salom %s!\n", local_name);
-    fclose(file);
+    printf("Salom ismingiz: ");
+    scanf("%s", name);
     srand(time(NULL));
     while (score[0] < 10 && score[1] < 10) {
         scanf("%s", input);
-        int result = checkWhichWon(input);
+        int result = check_winner(input);
         if (result == 1) score[0]++;
         else score[1]++;
         printf("%s\n", converter(result));
