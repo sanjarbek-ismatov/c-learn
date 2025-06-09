d8fa52ac132f7e4ed67e96ade1251979da8f5ab7
Sanjarbek <ismatovsanjarbek@yandex.ru>
2025-06-09 17:18:57 +0500

    reorganization

--- GeekforGeeks (updated)/1. C Basics/1.Compiler/main.c
+++
@@ -1,5 +0,0 @@
-#include<stdio.h>
-int main(){
-    printf("%d\n", (int)sizeof(unsigned long long int));
-    return 0;
-}
--- GeekforGeeks (updated)/1. C Basics/3.Escape Sequences/main.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main(){
-    printf("\a2\a3\a4");
-    return 0;
-}
