eb1753ea70ac2ff3d0a64aab5f68a0fa27fca0d8
Sanjarbek <ismatovsanjarbek@yandex.ru>
2025-06-10 22:04:38 +0500

    love.c: the history is being changed

--- K&R/main.c
+++ K&R/main.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 int main()
 {
-    /*
+
+    return 0;
+}
+
+void fahrenheit_to_celsius()
+{
     float farh, celsius;
     printf("Enter Fahrenheit: ");
     scanf("%f", &farh);
-    celsius = (5.0/9.0) * (farh - 32);
+    celsius = (5.0 / 9.0) * (farh - 32);
     printf("%.1f fahrenheit is %.1f Celsius gradus!", farh, celsius);
+}
 
-    char thechar = getchar();
-    putchar(thechar);
-    */
-
-    /*
+void putchar_examples()
+{
     int c, nc;
     while ((c = getchar()) != EOF)
     {
@@ -25,17 +28,17 @@ int main()
             printf("\n%d\n", ++nc);
         }
     }
-    */
 
-    /*
     for (nc = 1; (c = getchar()) != EOF; ++nc)
     {
         putchar(c);
         printf("%d\n", nc);
     }
     printf("%d\n", nc);
-    */
+}
 
+void word_count()
+{
     int ndigit[10], nwhite = 0, nother = 0;
     int c;
     for (int i = 0; i < 10; i++)
@@ -58,6 +61,4 @@ int main()
         else
             nother++;
     }
-
-    return 0;
 }
--- Old practices/love.c
+++ Old practices/love.c
@@ -1,35 +1,65 @@
 #include <stdio.h>
-#include <ctype.h>
 #include <string.h>
 #include <unistd.h>
-int main();
-int main(){
-    struct Person{
-        char name[10];
-        char isLove[3];
-        char herName[10];
-    };
-    struct Person person;
-    printf("Ismingiz nima: ");
-    scanf("%s", person.name);
-    printf("Salom \033[1;33m%s\033[0m, siz biror kishini sevasizmi? ", person.name);
-    scanf("%s", person.isLove);
+#include <stdbool.h>
+/**
+ * The origin of the code was dedicated to her, my ex-girlfriend
+ * The code's perspective was quite positive, but I wanted to change it as I already broke up with her
+ * (I know I was pathetic)
+ * You can the the original code from here the repository: (first code in May) https://github.com/sanjarbek-ismatov/c-learn/blob/c2c51a63890c7fafaa5694b3c296c3284f9a70d8/love.c , (updated code in August): https://github.com/sanjarbek-ismatov/c-learn/blob/2a9921c637c42c09c3cc6ecd87e1b8b5ea56d104/love.c
+ */
 
-    person.isLove[0] = tolower(person.isLove[0]);
-    while(strcmp(person.isLove, "ha") != 0){
-        printf("Meni alday olmaysiz!\nSevasizmi yo'qmi?  ");
-        scanf("%s", person.isLove);
+bool bool_convert(char bool_str[]);
+int main()
+{
+    struct Person
+    {
+        char name[20];
+        char her_name[20];
+        bool love;
+        bool career;
+    };
+
+    struct Person person;
+    char answer_str[10];
+    printf("Hey, What's your name?: ");
+    scanf("%s", person.name);
+
+    printf("\033[1;33m%s\033[0m, do you love anyone?: ", person.name);
+    scanf("%s", answer_str);
+    person.love = bool_convert(answer_str);
+
+    printf("Okay, \033[1;33m%s\033[0m, do you have your career?: ");
+    scanf("%s", answer_str);
+    person.career = bool_convert(answer_str);
+
+    while (person.love == true && person.career == false)
+    {
+        printf("!\nAre you okay with that?\nIt's not time to love, will you break up? ");
+        scanf("%s", answer_str);
+        person.love = bool_convert(answer_str);
     }
 
-        printf("Ajoyib, uning ismi nima? ");
-        scanf("%s", person.herName);
-        printf("Sevgingizning dajasi aniqlanmoqda...\n");
-        sleep(1);
-        printf("Aniqlandi!\n");
-         sleep(1);
-        if(strcmp(person.name, "Sanjarbek") == 0 || strcmp(person.name, "Jinnivoy") == 0)
-        printf("Siz har doim \033[1;36m%s\033[0mni kuchliroq sevasiz!!\n", person.herName);
-        else printf("Baribir ham \033[1;33m%s\033[0m sizni qattiqroq sevadi\n", person.herName);
-        printf("\033[1;33m%s\033[0m + \033[1;36m%s\033[0m = \033[1;32mLove!\033[0m, baxtli bo'linglar!!!!\n", person.name, person.herName);
+    printf("Great, what is your (future ex-)girlfriend's name? ");
+    scanf("%s", person.her_name);
+    printf("Your love to your career and girlfriend are being compared...\n");
+    sleep(1);
+    printf("Wait a minute!");
+    sleep(3);
+    printf("Analyzed!\n");
+    sleep(2);
+    if (person.career == true)
+        printf("You have always loved \033[1;36m yourself, future \033[0m most! Let's go bro!\n");
+    else
+        printf("You still have the chance to get up, f*ck \033[1;33m her \033[0m and go on!\n");
+    printf("Once you have full independence, you will find an \033[1;33m elite-minded \033[0m and \033[1;36m beautiful \033[0m woman. \033[1;32m At least \033[0m you will have build yourself up\n");
     return 0;
+}
+
+bool bool_convert(char bool_str[])
+{
+    if (strcmp(bool_str, "yes") == 0)
+        return true;
+    else
+        return false;
 }
