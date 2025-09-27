efba39374bbc0b3a42ef95ef1137ccdb4a4dd97b
Sanjarbek <ismatovsanjarbek@yandex.ru>
2025-09-27 07:06:22 +0500

    Modern Approach: Selection Statement Exercises (1-3)

---
+++ C Programming - Modern Approach/4.Selection Statements/Exercises/main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+int main() {
+  /* Section 5.1
+   * Exercise 1
+  int i = 2, j = 3x int k = i * j == 6;
+  printf("%d", k); // 1
+
+  int i = 5, j = 10, k = 1;
+  printf("%d", k > i < j); // 1
+  return 0;
+
+  int i = 3, j = 2, k = 1;
+  printf("%d", i < j == j < k); // 1
+
+  int i = 3, j = 4, k = 5;
+  printf("%d", i % j + i < k); // 1
+
+  * Exercise 2
+  int i = 10, j = 5;
+  printf("%d", !i < j); // 1
+
+  int i = 2, j = 1;
+  printf("%d", !!i + !j); // 1
+
+  int i = 5, j = 0, k = -5;
+  printf("%d", i && j || k); // 1 k = -5, but it is considered true
+
+  int i = 1, j = 2, k = 3;
+  printf("%d", i < j || k); // 1
+
+  * Exercise 3
+  int i = 3, j = 4, k = 5;
+  printf("%d \n", i < j || ++j < k); // 1
+  printf("%d %d %d\n", i, j, k);     // 3 4 5
+
+  i = 7;
+  j = 8;
+  k = 9;
+  printf("%d \n", i - 7 && j++ < k); // 0
+  printf("%d %d %d\n", i, j, k);     // 7 8 9
+
+  i = 7;
+  j = 8;
+  k = 9;
+  printf("%d \n", (i = j) || (j = k)); // 1
+  printf("%d %d %d\n", i, j, k);       // 8 8 9
+
+  i = 1;
+  j = 1;
+  k = 1;
+  printf("%d \n", ++i || ++j && ++k); // 1
+  printf("%d %d %d\n", i, j, k);      // 2 1 1
+  */
+}
