fc8ba190f684837acf146e71ee954cafc16bd85e
sanjarbek-ismatov <ismatovsanjarbek@yandex.ru>
2025-11-10 06:16:47 +0500

    7.1: Float Types

--- C Programming - Modern Approach/2.Formatted Input - Output/Section 3.1/print_specifiers.c
+++ C Programming - Modern Approach/2.Formatted Input - Output/Section 3.1/print_specifiers.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
   int x = 10;
   float f = 199.234f;
   printf("%-10.3d", x);
   printf("%.3d\n", x);
-  printf("%g\n", f);
+  printf("%.3g %e\n", f, f); // g prints the shortest way, either f or e. It also rounds instead of removing the numbers
   return 0;
 }
---
+++ C Programming - Modern Approach/7.Basic Types/7.2.Floating Types/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // 25.05
+    float num = 25.05E0; // 0.02505E3; 2505e-2;
+    printf("%f\n", num);
+    return 0;
+}
