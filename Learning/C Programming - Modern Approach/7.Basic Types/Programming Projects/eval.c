4074a796a9f06e73fcf637764361963def886c1e
sanjarbek-ismatov <ismatovsanjarbek@yandex.ru>
2025-12-07 06:56:37 +0500

    CPMA: eval.c changes (not fixed, but close to the solution)

--- Learning/C Programming - Modern Approach/7.Basic Types/Programming Projects/eval.c
+++ Learning/C Programming - Modern Approach/7.Basic Types/Programming Projects/eval.c
@@ -8,46 +8,56 @@ float power(int base, short degree) {
   return calculated;
 }
 
+void calculate_expression(float *operand1, float operand2, char operator) {
+  switch (operator) {
+  case '+':
+    *operand1 += operand2;
+    break;
+  case '-':
+    *operand1 -= operand2;
+    break;
+  case '*':
+    *operand1 *= operand2;
+    break;
+  case '/':
+    *operand1 /= operand2;
+    break;
+  }
+}
 int main() {
-  char c;
-  float current_num = 0.0f, result = 0.0f;
+  char c, operator;
+  float first_operand = 0.0f, second_operand = 0.0f, current_number = 0.0f;
   short point_count = 0;
-  bool is_float = false;
+  bool is_float = false, is_first_operand = true;
   printf("Enter an expression: ");
   while ((c = getchar()) != '\n') {
     if (c >= '0' && '9' >= c) {
       short number = c - '0';
       if (is_float)
-        current_num += number / power(10, ++point_count);
+        current_number += number / power(10, ++point_count);
       else {
-        current_num *= 10.0f;
-        current_num += number;
+        current_number *= 10.0f;
+        current_number += number;
       }
     } else if (c == '.') {
       is_float = true;
-    } else {
-      switch (c) {
-      case '+':
-        result += current_num;
-        break;
-      case '-':
-        result -= current_num;
-        break;
-      case '*':
-        result *= current_num;
-        break;
-      case '/':
-        result /= current_num;
-        break;
-      default:
-        fprintf(stderr, "Invalid operator!");
-        return 1;
+    } else if (c == '+' || c == '-' || c == '*' || c == '/') {
+      operator = c;
+      if (is_first_operand) {
+        is_first_operand = false;
+        first_operand = current_number;
+      } else {
+        printf("%f\n", current_number);
+        second_operand = current_number;
+        calculate_expression(&first_operand, second_operand, operator);
       }
+      current_number = 0.0f;
       point_count = 0;
       is_float = false;
-      current_num = 0.0f;
+    } else {
+      fprintf(stderr, "Invalid expression!");
     }
   }
-  printf("Value of expression: %f\n", result);
+  printf("Value of expression: %f\n", first_operand);
   return 0;
 }
