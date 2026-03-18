ec3be5857b8ec69956e58e190c036eb5a31d1742
sanjarbek-ismatov <ismatovsanjarbek@yandex.ru>
2026-03-18 11:50:36 +0500

    quick sort trial

--- Learning/C Programming - Modern Approach/9.Functions/Programs/quick_sort.c
+++ Learning/C Programming - Modern Approach/9.Functions/Programs/quick_sort.c
@@ -1,20 +1,15 @@
-#include <stdio.h>
-#define LEN 20
-int *quick_sort(int arr[], int length)
-{
+int* quick_sort(int arr[], int start, int end){
+    int* left = quick_sort(arr, 0, end);
+    int* right = quick_sort(arr, 0, end);
 }
 
-int main(void)
-{
-    int arr[LEN] = {
-        73, 14, 92, 5, 41,
-        67, 28, 89, 36, 11,
-        54, 99, 2, 76, 33,
-        48, 21, 64, 87, 10};
-    int result[] = quick_sort(arr, LEN);
-    for (int i = 0; i < LEN; i++)
-    {
-        printf("%d ", result[i]);
+void sort(int arr[], int start, int end){
+    int low = start, high = end;
+    int pivot = arr[low];
+    while(low <= high){
+        if(high-- < pivot)
+            arr[low] = arr[high];
+        else if (low++ > pivot)
+            arr[high] = arr[low];
     }
-    return 0;
 }
